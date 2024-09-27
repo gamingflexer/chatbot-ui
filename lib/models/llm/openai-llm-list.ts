b8fae45951f23646b0b4467bc0dcac5e312ef215
Om Surve <50608241+gamingflexer@users.noreply.github.com>
2024-09-27 14:02:32 +0530

    updated model names

--- lib/models/llm/openai-llm-list.ts
+++ lib/models/llm/openai-llm-list.ts
@@ -4,17 +4,17 @@ const OPENAI_PLATORM_LINK = "https://platform.openai.com/docs/overview"
 
 // OpenAI Models (UPDATED 1/25/24) -----------------------------
 const GPT4o: LLM = {
-  modelId: "gpt-4o",
-  modelName: "GPT-4o",
+  modelId: "gpt-4o-2024-08-06",
+  modelName: "gpt-4o-2024-08-06",
   provider: "openai",
-  hostedId: "gpt-4o",
+  hostedId: "gpt-4o-2024-08-06",
   platformLink: OPENAI_PLATORM_LINK,
   imageInput: true,
   pricing: {
     currency: "USD",
     unit: "1M tokens",
-    inputCost: 5,
-    outputCost: 15
+    inputCost: 2.5,
+    outputCost: 7.5
   }
 }
 
